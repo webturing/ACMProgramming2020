1628cc60058b2cea90420ffc13dd7b4fb999cfef
webturing <zj@webturing.com>
2020-06-07 10:22:02 +0800

    更新了C++11的比赛模板 1. 万能头文件 2. 64位整数ll 3. 方便读入read 4. 改进cin/cout的速度（取消和scanf/printf同步等） 5. 简单的正向反向循环 FOR/FORD

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#pragma comment(linker, "/stack:247474112")
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+
+template<typename T=int>
+inline void oo(string str, T val) { cerr << str << val << endl; }
+
+template<typename T=int>
+inline T read() {
+    T x;
+    cin >> x;
+    return x;
+}
+
+#define endl '\n'
+#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
+#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
+
+int main() {
+    std::iostream::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+
+    return 0;
+}
