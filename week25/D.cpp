a6b1a251e8dc16deeac938743933d8ecfe84e434
webturing <zj@webturing.com>
2020-06-14 20:48:10 +0800

    机器指令：字符串、16进制转2进制

---
+++ week25/D.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    vector<string> v(istream_iterator<string>(cin), (istream_iterator<string>()));
+    for_each(v.begin(), v.end(), [](string s) {
+
+        for_each(s.begin(), s.end(), [](char c) {
+            string st;
+            st.push_back(c);
+            istringstream iss(st);
+            int x;
+            iss >> hex >> x;
+            for (auto i = 3; i >= 0; i--) {
+                cout << ((x >> i) & 1);
+            }
+        });
+        cout << endl;
+    });
+    return 0;
+}
