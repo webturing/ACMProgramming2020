d88519c8d70b0ad1a593795c74a7bec77082035e
webturing <zj@webturing.com>
2020-06-21 16:30:39 +0800

    week25 C++11 题解 最大子序列

--- week25/A.cpp
+++ week25/A.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     vector<string> v(istream_iterator<string>(cin), (istream_iterator<string>()));
-    int tot = 0;
+    int tot(0);
     for_each(v.begin(), v.end(), [&tot](string s) -> bool {
         if (s.find('4') == string::npos and count_if(s.begin(), s.end(), [](char c) -> bool {
             return c == '6' or c == '8';
