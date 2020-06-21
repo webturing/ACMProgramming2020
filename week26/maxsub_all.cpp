a9ef5477e834e2cab134768251a33450464e10a2
webturing <zj@webturing.com>
2020-06-21 09:11:40 +0800

    weekly26th 最大子序列的五种解法

--- week26/maxsub_all.cpp
+++ week26/maxsub_all.cpp
@@ -30,7 +30,6 @@ inline T read() {
 #define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
 #define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
 
-//最容易想到的模拟算法 枚举每一个子序列O(n^3)
 int max_sub_cube(vector<int> &v) {//O(n^3)
     int best = 0, n = int(v.size());
     for (int i = 0; i < n; i++)
@@ -44,10 +43,11 @@ int max_sub_cube(vector<int> &v) {//O(n^3)
     return best;
 }
 
-//第一个优化，预处理前缀和，然后可以在O(1)时间内计算出子序列v[i:k] 整体时间O(n^2)
 int max_sub_square(vector<int> &v) {//O(n^2)
     vector<int> b(v);
+    oo("pre=", b);
     partial_sum(b.begin(), b.end(), b.begin());//O(n)
+    oo("post=", b);
     int best = 0, n = int(v.size());
     for (int i = 0; i < n; i++)
         for (int j = i + 1; j < n; j++) {
@@ -67,7 +67,7 @@ int max_sub_linear_dp(vector<int> &v) {//O(n)
 }
 
 int max_sub_linear_greedy(vector<int> &v) {//O(n)
-    int best(0), local(0);//如果数组全负有的要求返回最大值，也就一个O(n)判断 也可以用best=INT_MIN来初始化
+    int best(0), local(0);//如果数组全负有的要求返回最大值，也就一个O(n)判断,也可以用best=INT_MIN来初始化
     for (auto i = 0; i < v.size(); i++) {
         local += v[i];
         if (local < 0) {
@@ -76,6 +76,24 @@ int max_sub_linear_greedy(vector<int> &v) {//O(n)
         best = max(best, local);
     }
     return best;
+}
+
+int max_sub_dc(vector<int> &v, int left, int right) {//O(nlog(n))
+    if (left == right)return v[left];
+    int mid = (left + right) / 2;
+    int max_left = max_sub_dc(v, left, mid);
+    int max_right = max_sub_dc(v, mid + 1, right);
+    int max_cross_left(0), max_cross_right(0), sum_left(0), sum_right(0);
+    for (int i = mid; i >= left; i--) {
+        sum_left += v[i];
+        max_cross_left = max(sum_left, max_cross_left);
+    }
+    for (int i = mid + 1; i <= right; i++) {
+        sum_right += v[i];
+        max_cross_right = max(sum_right, max_cross_right);
+    }
+
+    return max(max(max_left, max_right), max_cross_left + max_cross_right);
 }
 
 int main() {
@@ -88,6 +106,6 @@ int main() {
     cout << max_sub_square(v) << endl;
     cout << max_sub_linear_dp(v) << endl;
     cout << max_sub_linear_greedy(v) << endl;
-    cout << max_sub_square_dc(v) << endl;
+    cout << max_sub_dc(v, 0, int(v.size()) - 1) << endl;
     return 0;
 }
