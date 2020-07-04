4a012a32b1f998f8095a2fe82fb502a94c48cbed
webturing <zj@webturing.com>
2020-07-04 16:10:37 +0800

    位操作应用

---
+++ week27/E.cpp
@@ -0,0 +1,38 @@
+#pragma comment(linker, "/stack:247474112")
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+
+template<typename T=int>
+inline void oo(const string &str, T val) { cerr << str << val << endl; }
+
+template<typename T=int>
+inline T read() {
+    T x;
+    cin >> x;
+    return x;
+}
+
+#define endl '\n'
+#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
+#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    for (size_t n; cin >> n;) {
+        int tot(0);
+        for (int i = 0; n >> i; i++) {
+            tot += (n >> i) & 1;
+        }
+        cout << (tot & 1 ? "odd" : "even") << endl;
+    }
+
+
+    return 0;
+}
