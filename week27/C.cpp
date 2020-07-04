df188747271b335057ebff37f715d7f978ae44a1
webturing <zj@webturing.com>
2020-07-04 15:37:38 +0800

    朴素字符串匹配s.find(t,pos)

---
+++ week27/C.cpp
@@ -0,0 +1,37 @@
+#pragma comment(linker, "/stack:247474112")
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll=long long;
+
+template<typename T=int>
+inline void oo(const string &str, T val) { cerr << str << val << endl; }
+
+template<typename T=int>
+inline T read() {
+    T x;
+    cin >> x;
+    return x;
+}
+
+#define endl '\n'
+#define FOR(i, x, y) for (decay<decltype(y)>::type i = (x), _##i = (y); i < _##i; ++i)
+#define FORD(i, x, y) for (decay<decltype(x)>::type i = (x), _##i = (y); i > _##i; --i)
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    for (string s, t; cin >> s >> t;) {
+        size_t tot = 0, pos = 0;
+        for (; (pos = s.find(t, pos)) != string::npos; pos += t.size()) {
+            ++tot;
+        }
+        cout << tot << endl;
+    }
+
+    return 0;
+}
